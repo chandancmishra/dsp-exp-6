8c2fb3b0e6c1c9a431cfdd90aa0f6d2e5eb117e5
chandancmishra <chandan5108@gmail.com>
2023-05-29 11:22:28 +0530

    updated graph

--- Exp1S/js/adidspex6s.js
+++ Exp1S/js/adidspex6s.js
@@ -248,7 +248,7 @@ b43 = new LeaderLine(b4, LeaderLine.pointAnchor(b3, {x: '0%', y: '80%'}),
                         color: '#7f7f7f'
                       },
                       xaxis: {
-                        title:'Frequency (Khz)',
+                        title:'Frequency (KHz)',
                         showticklabels: false,
                         autotick: true},
                         yaxis: {
@@ -266,7 +266,7 @@ b43 = new LeaderLine(b4, LeaderLine.pointAnchor(b3, {x: '0%', y: '80%'}),
                         color: '#7f7f7f'
                       },
                       xaxis: {
-                        title:'Time (s)',
+                        title:'Frquency/Time (s)',
                         showticklabels: false,
                         autotick: true},
                         yaxis: {
